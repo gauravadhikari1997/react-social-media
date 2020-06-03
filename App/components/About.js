478395ebb2e7b7a27e8d377c6cdd43b460fa58e6
unknown <gaurav.15bme2023@abes.ac.in>
2020-06-04 00:35:21 +0530

    updated about and terms page

--- App/components/About.js
+++ App/components/About.js
@@ -8,20 +8,7 @@ function About() {
       <p className="lead text-muted">
         About Page of the React Social Media App.
       </p>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. At qui enim
-        rem totam voluptatum. Aut saepe temporibus, facilis ex a iste expedita
-        minima dolorum dicta doloribus libero aliquid, quae maxime? Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Fugiat suscipit beatae eum,
-        est soluta ducimus ratione et impedit sapiente, nihil, atque dignissimos
-        adipisci? Totam atque officia quis voluptates sed veniam?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
-        voluptates quisquam possimus tenetur, dicta enim rerum quis, quaerat id
-        nobis provident quo dolorum sapiente temporibus facere non repellendus
-        consequatur cupiditate!
-      </p>
+      <p>Here you can write posts, follow people and chat with others.</p>
     </Page>
   );
 }
